ed2635611cf97a631440aab7c61b03bd2959eb4f
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-03-16 13:27:04 +0100

    Factory for database
    initiates the illuminate database and returns its capsule instance

---
+++ Factory.php
@@ -0,0 +1,28 @@
+<?php
+namespace SlaxWeb\Database;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+class Factory
+{
+    public static function Database(&$CI)
+    {
+        $capsule = new Capsule();
+
+        $capsule->addConnection(
+            'driver'    =>  $CI->conf->item("db_driver"),
+            'host'      =>  $CI->conf->item("db_host"),
+            'database'  =>  $CI->conf->item("db_name"),
+            'username'  =>  $CI->conf->item("db_user"),
+            'password'  =>  $CI->conf->item("db_pass"),
+            'charset'   =>  $CI->conf->item("db_charset"),
+            'collation' =>  $CI->conf->item("db_collation"),
+            'prefix'    =>  $CI->conf->item("db_prefix")
+        );
+
+        $capsule->setEventDispatcher(new Dispatcher(new Container));
+        return $capsule;
+    }
+}
